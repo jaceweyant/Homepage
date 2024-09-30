0da5e5f3cf9283147416978964078cafe98caada
Jace Weyant <ja.weyant0@gmail.com>
2024-09-30 16:17:05 -0400

    New VAO, new Model class with vao and material

--- components/Model2.js
+++ components/Model2.js
@@ -1,11 +1,18 @@
-class Model_OLD {
-	constructor(meshData){
-		this.transform = new Transform();
-		this.color = new Vector3(1,1,1);
-		this.mesh = meshData;
-	}
+class Model {
+    constructor(vao, material) {
+        this.transform = new Transform();
+        this.vao = vao;
+        this.visible = true;
+        this.material = material;
+        this.color = new Vector3();
+    }
 
-	//--------------------------------------------------------------------------
+    draw() {
+        if(this.vao.isIndexed)	gl.drawElements(this.material.drawMode, this.vao.count, gl.UNSIGNED_SHORT, 0); 
+        else					gl.drawArrays(this.material.drawMode, 0, this.vao.count);
+    }
+
+    //--------------------------------------------------------------------------
 	//Getters/Setters
 	setScale(x,y,z){ this.transform.scale.set(x,y,z); return this; }
 	setPosition(x,y,z){ this.transform.position.set(x,y,z); return this; }
