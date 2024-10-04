e8e0d087433246636d43d04306b51c6ad0575b6b
Jace Weyant <ja.weyant0@gmail.com>
2024-10-04 13:59:09 -0400

    Lots of changes to how the renderer works, currently issues with uniforms link to program

--- rendering/Renderer.js
+++ rendering/Renderer.js
@@ -32,3 +32,56 @@ class Renderer {
 		return this;		
 	}
 }
+
+// Takes in an array of models/renderables, prepares the buffers and uniforms, and renders
+var Render = (function() {
+    var material = shader = null;
+
+    var f = function(renderingObj) {
+        var gl = renderingObj.gl,
+            ary = renderingObj.renderables,
+            components = {};
+            uniformsAry = [];
+
+        components.camera = renderingObj.camera;
+        components.light = renderingObj.light;
+
+        for (var i=0; i<ary.length; i++) {
+            if (!ary[i].visible) continue;
+
+            components.model = ary[i];
+
+            uniformsAry = renderingObj.getUniforms(components);
+
+            ary[i].updateUniforms(uniformsAry);
+
+            //Check if the next material to use is different from the last
+            if (material !== ary[i].material) {
+                material = ary[i].material;
+
+                if (!material) {console.error("material: " + material + " not found"); return this};
+
+				//Multiple materials can share the same shader, if new shader, turn it on.
+                if (material.shader != shader) shader = material.shader.activate();
+
+                //Turn on/off any gl features
+				//if(material.useCulling != CULLING_STATE)	gl[ ( (CULLING_STATE = (!CULLING_STATE))  )?"enable":"disable" ](gl.CULL_FACE);
+				//if(material.useBlending != BLENDING_STATE)	gl[ ( (BLENDING_STATE = (!BLENDING_STATE)) )?"enable":"disable" ](gl.BLEND);
+            }
+
+            //Prepare Buffers and Uniforms.
+            gl.bindVertexArray(ary[i].vao.id);
+            //if (material.useModelMatrix) material.shader.setUniforms("", ary[i].updateMatrix());
+
+            //Render 
+            if (ary[i].vao.isIndexed)  material.gl.drawElements(material.drawMode, ary[i].vao.count, gl.UNSIGNED_SHORT, 0);
+            else                       material.gl.drawArrays(material.drawMode, 0, ary[i].vao.count);
+
+        }
+
+        //Cleanup
+        gl.bindVertexArray(null);
+    };
+
+    return f;
+})();
